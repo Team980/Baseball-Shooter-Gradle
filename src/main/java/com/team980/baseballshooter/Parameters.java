9cbbff91b58704fac3f8c6b0401e4db32d2cb45b
Luke Myers <19lmyers@gmail.com>
2016-10-22 10:46:19 -0700

    Baseball Shooter code in a new and exciting form

---
+++ src/main/java/com/team980/baseballshooter/Parameters.java
@@ -0,0 +1,45 @@
+package com.team980.baseballshooter;
+
+public class Parameters {
+
+    // --- DRIVE SYSTEM ---
+    public static int leftDriveMotorChannel = 0;
+    public static int rightDriveMotorChannel = 1;
+    public static double maxDriveOutput = 0.4;
+
+    public static double leftMotorMultiplier = 1.0; //TODO determine these values
+    public static double rightMotorMultiplier = 1.0; //TODO based on encoder tuning
+
+    // --- AUTONOMOUS ---
+    public static double autoDistance = 15.0; //feet
+    public static double autoSpeed = 1.0;
+
+    // --- RELAYS ---
+    public static int winchRelayChannel = 0;
+    public static int actuatorRelayChannel = 1;
+
+    // --- ENCODERS ---
+    public static int leftDriveEncA = 0;
+    public static int leftDriveEncB = 1;
+    public static boolean leftDriveEncInv = false; //TODO
+
+    public static int rightDriveEncA = 2;
+    public static int rightDriveEncB = 3;
+    public static boolean rightDriveEncInv = false; //TODO
+
+    public static int driveEncoderCounts = -1; //TODO
+
+    // --- TIMINGS ---
+    public static double actuatorStopTime = 1.9;
+
+    // --- JOYSTICKS / DRIVER INPUTS ---
+    public static int driveJsChannel = 0;
+
+    public static int driveJsWinchPullButton = 3;
+    public static int driveJsWinchReleaseButton = 4;
+
+    public static int driveJsTriggerButton = 1;
+    public static int driveJsFailsafeButton = 9;
+    public static int driveJsRetractButton = 2;
+    public static int driveJsEStopButton = 5; //for testing
+}
---
+++ src/main/java/com/team980/baseballshooter/Robot.java
@@ -0,0 +1,130 @@
+package com.team980.baseballshooter;
+
+import edu.wpi.first.wpilibj.*;
+
+public class Robot extends IterativeRobot {
+
+    private RobotDrive robotDrive;
+
+    private Joystick driveStick;
+
+    private Relay winchRelay;
+    private Relay actuatorRelay;
+
+    private boolean canFire = true;
+    private boolean firing = false;
+    private Timer firingTimer;
+    private double stopTime;
+
+    //private Encoder leftDriveEnc;
+    //private Encoder rightDriveEnc;
+
+    public Robot() {
+        robotDrive = new RobotDrive(Parameters.leftDriveMotorChannel, Parameters.rightDriveMotorChannel);
+        robotDrive.setMaxOutput(Parameters.maxDriveOutput);
+
+        driveStick = new Joystick(Parameters.driveJsChannel);
+
+        winchRelay = new Relay(Parameters.winchRelayChannel);
+        actuatorRelay = new Relay(Parameters.actuatorRelayChannel);
+
+        //leftDriveEnc = new Encoder(Parameters.leftDriveEncA, Parameters.leftDriveEncB);
+        //leftDriveEnc.setDistancePerPulse(2*Constants.pi*(Constants.wheelRadius/Constants.inchesInFeet)/Parameters.driveEncoderCounts); TODO figure out the calculations for this
+        //leftDriveEnc.setReverseDirection(Parameters.leftDriveEncInv);
+        //rightDriveEnc = new Encoder(Parameters.rightDriveEncA, Parameters.rightDriveEncB);
+        //rightDriveEnc.setDistancePerPulse(2*Constants.pi*(Constants.wheelRadius/Constants.inchesInFeet)/Parameters.driveEncoderCounts); TODO crunch the numbers
+        //rightDriveEnc.setReverseDirection(Parameters.rightDriveEncInv);
+
+        firingTimer = new Timer();
+    }
+
+    public void robotInit() {
+        //leftDriveEnc.reset();
+        //rightDriveEnc.reset();
+    }
+
+    public void autonomousInit() {
+
+    }
+
+    public void autonomousPeriodic() {
+    	
+    	/*double currentDistLeft = leftDriveEnc.getDistance();
+    	double currentDistRight = rightDriveEnc.getDistance();
+    	
+    	if (currentDistLeft > Parameters.autoDistance &&
+    			currentDistRight > Parameters.autoDistance) {
+    		robotDrive.setLeftRightMotorOutputs(0, 0); //stops the robot
+    	} else {
+    		robotDrive.setLeftRightMotorOutputs(Parameters.leftMotorMultiplier * Parameters.autoSpeed, 
+    				Parameters.rightMotorMultiplier * Parameters.autoSpeed);
+    	}
+    	
+    	SmartDashboard.putNumber("Current Distance - Left", currentDistLeft);
+    	SmartDashboard.putNumber("Current Distance - Right", currentDistRight);*/
+    }
+
+    public void teleopInit() {
+        firingTimer.start();
+    }
+
+    public void teleopPeriodic() {
+        System.out.println("laser cannon ready");
+
+        if (driveStick.getRawButton(Parameters.driveJsWinchPullButton)) {
+            //Pull the winch
+            winchRelay.set(Relay.Value.kForward);
+            //System.out.println("Relay FORWARD");
+        } else if (driveStick.getRawButton(Parameters.driveJsWinchReleaseButton)) {
+            //Release the winch
+            winchRelay.set(Relay.Value.kReverse);
+            //System.out.println("Relay REVERSE");
+        } else {
+            //Do nothing with the winch
+            winchRelay.set(Relay.Value.kOff);
+        }
+
+        if (driveStick.getRawButton(Parameters.driveJsTriggerButton) && driveStick.getRawButton(Parameters.driveJsFailsafeButton)
+                && canFire) {
+            //Fire the baseball!
+            firing = true;
+            canFire = false;
+
+            stopTime = firingTimer.get() + Parameters.actuatorStopTime;
+            System.out.println("FIRE THE CANNON");
+        }
+
+        if (firing && ((firingTimer.get() >= stopTime) || (driveStick.getRawButton(Parameters.driveJsEStopButton)))) {
+            firing = false;;
+
+            System.out.println("STOP THE CANNON");
+        }
+
+        if (firing) {
+            //Fire the cannon!
+            actuatorRelay.set(Relay.Value.kForward);
+            System.out.println("FIRING");
+        } else if (driveStick.getRawButton(Parameters.driveJsRetractButton) && !driveStick.getRawButton(Parameters.driveJsEStopButton)) {
+            //Bring the actuator back
+            canFire = true;
+            actuatorRelay.set(Relay.Value.kReverse);
+            System.out.println("REVERSE");
+        } else {
+            actuatorRelay.set(Relay.Value.kOff);
+            System.out.println("NOPE");
+        }
+
+        //Custom inputs to fix the turning
+        robotDrive.arcadeDrive(driveStick.getY(), driveStick.getZ() * -1);
+    }
+
+    public void disabledInit() {
+        winchRelay.set(Relay.Value.kOff);
+        actuatorRelay.set(Relay.Value.kOff);
+    }
+
+    public void testPeriodic() {
+
+    }
+
+}
